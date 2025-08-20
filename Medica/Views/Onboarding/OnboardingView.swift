ca6e3ef0f0d1c2fe673be059205d8593d22285c4
Oluwatobiloba Akinrujomu <tobiak2011@gmail.com>
2025-08-20 09:55:01 +0200

    Project Setup, Implemented Onboarding screen

---
+++ Medica/Views/Onboarding/OnboardingView.swift
@@ -0,0 +1,43 @@
+//
+//  OnboardingView.swift
+//  Medica
+//
+//  Created by Oluwatobiloba Akinrujomu on 20/08/2025.
+//
+
+import SwiftUI
+
+struct OnboardingView: View {
+    
+    @State var isActive: Bool = false
+    
+    var body: some View {
+        ZStack {
+            if self.isActive {
+                LoginView()
+            } else {
+                VStack {
+                    Image("Medinova")
+                    Text("Medica")
+                        .foregroundStyle(.colorPrimary)
+                        .fontWeight(.semibold)
+                        .font(.system(size: 70, weight: .bold, design: .rounded))
+                    //.font(.system(.largeTitle, design: .rounded, weight: .semibold))
+                    
+                }
+            }
+            
+        }.onAppear {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 3, execute: {
+                withAnimation {
+                    self.isActive = true
+                }
+            })
+        }
+        
+    }
+}
+
+#Preview {
+    OnboardingView()
+}
