1858d4775466f246820a53089f56177380874716
Oluwatobiloba Akinrujomu <tobiak2011@gmail.com>
2025-08-20 09:10:12 +0200

    Initial Commit

---
+++ Medica/MedicaApp.swift
@@ -0,0 +1,17 @@
+//
+//  MedicaApp.swift
+//  Medica
+//
+//  Created by Oluwatobiloba Akinrujomu on 20/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct MedicaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
